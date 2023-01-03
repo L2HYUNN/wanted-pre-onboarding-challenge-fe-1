b53691fc4b0a33958e7808dd2fd42067124fd879
L2HYUNN <donghyuni98@gmail.com>
2023-01-03 20:15:28 +0900

    feat: add global reset style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <Routes>
+      <GlobalStyles />
       <Route path="/" element={<Home />} />
     </Routes>
   );
