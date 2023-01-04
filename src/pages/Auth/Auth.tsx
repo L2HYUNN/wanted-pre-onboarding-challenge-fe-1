25b6c948bc04ad9a7ffe4f53a4f8056d099030f7
L2HYUNN <donghyuni98@gmail.com>
2023-01-04 17:18:41 +0900

    feat: add basic page components

---
+++ src/pages/Auth/Auth.tsx
@@ -0,0 +1,10 @@
+/**
+ * 토큰 여부를 확인한다.
+ * 1. 토큰이 있을시 -> Login 컴포넌트
+ * 2. 토큰이 없을시 -> Join 컴포넌트
+ * @returns
+ */
+const Auth = () => {
+  return <>Hello</>;
+};
+export default Auth;
---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <>
+      <p>Unexpected error has occurred.</p>
+    </>
+  );
+};
+
+export default Error;
