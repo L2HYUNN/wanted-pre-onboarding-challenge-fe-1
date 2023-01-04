2b6775f633015979034a43b8e0a05acb2e834522
L2HYUNN <donghyuni98@gmail.com>
2023-01-04 17:19:30 +0900

    feat: add basic router setting

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/Layout";
+import Auth from "../pages/Auth";
+import Error from "../pages/Error";
 import Home from "../pages/Home";
+import Join from "../pages/Join";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "auth", element: <Auth /> },
+      { path: "join", element: <Join /> },
+      { path: "login", element: <Login /> },
+    ],
   },
 ]);
 
