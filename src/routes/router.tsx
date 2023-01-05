db63a59e1c1fe4abbea25522d272536480cb3130
L2HYUNN <donghyuni98@gmail.com>
2023-01-05 16:21:36 +0900

    refactor: remove auth page

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
-import Auth from "../pages/Auth";
 import Error from "../pages/Error";
 import Home from "../pages/Home";
 import Join from "../pages/Join";
@@ -16,7 +15,6 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { index: true, element: <Home /> },
-      { path: "auth", element: <Auth /> },
       { path: "join", element: <Join />, action: joinAction },
       {
         path: "login",
