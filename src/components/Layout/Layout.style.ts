b3d1ed572875591730096306dc991e5d0d0e1a9e
L2HYUNN <donghyuni98@gmail.com>
2023-01-05 13:29:00 +0900

    style: add CommonStyles, Layout styles

--- src/components/Layout/Layout.style.ts
+++ src/components/Layout/Layout.style.ts
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  justify-content: center;
   width: 100%;
   height: 100%;
+  padding-top: 100px;
 `;
---
+++ src/styles/CommonStyles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  width: 150px;
+  margin-bottom: 5px;
+`;
+
+export const Button = styled.button`
+  width: 158px;
+`;
+
+export const ErrorMessage = styled.p`
+  color: #e44c42;
+  font-size: 12px;
+  font-weight: 600;
+`;
