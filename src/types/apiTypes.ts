602deb7920ab77407c07556476703e47e777f262
L2HYUNN <donghyuni98@gmail.com>
2023-01-06 18:03:14 +0900

    feat: add api types

--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -6,3 +6,19 @@ export interface PostFormReturnProps {
   message: string;
   token: string;
 }
+
+export interface PostToDoReturnProps {
+  title: string;
+  content: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface PostToDoReturnProp {
+  data: PostToDoReturnProps;
+}
+
+export interface DeleteToDoProps {
+  data: null;
+}
