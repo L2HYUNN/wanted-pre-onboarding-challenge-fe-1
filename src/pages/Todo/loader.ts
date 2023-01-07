cc4362f18803daa367c9861b4c96af067747a046
L2HYUNN <donghyuni98@gmail.com>
2023-01-07 17:39:48 +0900

    feat: add check & redirect user not have token

--- src/pages/Todo/loader.ts
+++ src/pages/Todo/loader.ts
@@ -1,8 +1,13 @@
 import { QueryClient } from "@tanstack/react-query";
+import { redirect } from "react-router-dom";
 import { getTodosQuery } from "../../apis/queries";
+import tokenLoader from "../../utils/tokenLoader";
 
 const loader = (queryClient: QueryClient) => async () => {
   const query = getTodosQuery();
+  const token = tokenLoader();
+
+  if (!token) return redirect("/login");
 
   if (query.queryKey) {
     return (
