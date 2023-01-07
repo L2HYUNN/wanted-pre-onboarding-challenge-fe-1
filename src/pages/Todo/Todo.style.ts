5d3f5717407ecc47849afa254242846d688f74e2
L2HYUNN <donghyuni98@gmail.com>
2023-01-07 17:40:23 +0900

    feat: add logout button & logic

--- src/pages/Todo/Todo.style.ts
+++ src/pages/Todo/Todo.style.ts
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  height: 100%;
+`;
+
+export const TodoContainer = styled.div`
   display: flex;
   justify-content: space-between;
   /* align-items: center; */
   width: 600px;
   height: 100%;
+`;
+
+export const TodoLogOutButton = styled.button`
+  margin-bottom: 20px;
 `;
--- src/pages/Todo/Todo.tsx
+++ src/pages/Todo/Todo.tsx
@@ -1,10 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
-import { Outlet, useFetcher, useLoaderData } from "react-router-dom";
+import {
+  Outlet,
+  useFetcher,
+  useLoaderData,
+  useNavigate,
+} from "react-router-dom";
 import { getTodosQuery } from "../../apis/queries";
 import TodoList from "../../components/Todo/TodoList";
 import * as CS from "../../styles/CommonStyles";
 import * as S from "./Todo.style";
 import loader from "./loader";
+import { useCallback } from "react";
 
 const Todo = () => {
   // FIXME: need to change unknown typing
@@ -17,25 +23,36 @@ const Todo = () => {
     },
   } = useQuery({ ...getTodosQuery(), initialData }) as any;
   const fetcher = useFetcher();
+  const navigate = useNavigate();
+
+  const handleLogOutButton = useCallback(() => {
+    localStorage.clear();
+    navigate("/login");
+  }, [navigate]);
 
   // TODO: need to optimize rendering
   // FIXME: input value isn't initailized after submitting Form.
   return (
     <S.Container>
-      <TodoList list={data} />
-      <Outlet />
-      <fetcher.Form
-        method="post"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          marginTop: "16px",
-        }}
-      >
-        <CS.Input type="text" name="title" placeholder="title" />
-        <CS.Textarea name="content" placeholder="content" />
-        <CS.Button type="submit">Register</CS.Button>
-      </fetcher.Form>
+      <S.TodoLogOutButton type="button" onClick={handleLogOutButton}>
+        Log out
+      </S.TodoLogOutButton>
+      <S.TodoContainer>
+        <TodoList list={data} />
+        <Outlet />
+        <fetcher.Form
+          method="post"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginTop: "16px",
+          }}
+        >
+          <CS.Input type="text" name="title" placeholder="title" />
+          <CS.Textarea name="content" placeholder="content" />
+          <CS.Button type="submit">Register</CS.Button>
+        </fetcher.Form>
+      </S.TodoContainer>
     </S.Container>
   );
 };
