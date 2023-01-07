cbfc2a86e63947391c14e857cfc9c912e8f12174
L2HYUNN <donghyuni98@gmail.com>
2023-01-07 11:27:44 +0900

    feat: add TodoList component

--- src/components/Todo/TodoList/TodoList.style.ts
+++ src/components/Todo/TodoList/TodoList.style.ts
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
 export const TodoList = styled.ul`
   display: flex;
   flex-direction: column;
+`;
+
+export const TodoTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 500;
+  margin-bottom: 15px;
 `;
--- src/components/Todo/TodoList/TodoList.tsx
+++ src/components/Todo/TodoList/TodoList.tsx
@@ -1,14 +1,23 @@
 import { PostToDoReturnProps } from "../../../types/apiTypes";
-import TodoItem from "../TodoItem";
+import TodoIndex from "../TodoIndex";
 import * as S from "./TodoList.style";
 
 const TodoList = ({ list }: { list: PostToDoReturnProps[] }) => {
   return (
-    <S.TodoList>
-      {list.map((todo) => {
-        return <TodoItem key={todo.id} id={todo.id} item={todo}></TodoItem>;
-      })}
-    </S.TodoList>
+    <S.Container>
+      <S.TodoTitle>ToDo List</S.TodoTitle>
+      <S.TodoList>
+        {list.map((todo) => {
+          return (
+            <TodoIndex
+              key={todo.id}
+              id={todo.id}
+              title={todo.title}
+            ></TodoIndex>
+          );
+        })}
+      </S.TodoList>
+    </S.Container>
   );
 };
 
